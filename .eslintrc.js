4c8179d8bf62e1acb33012ec2bc97106dde93ee7
Badis Merabet <merabetbadis@live.fr>
2017-12-07 15:20:56 +0100

    Setup testing tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,19 +20,19 @@ module.exports = {
     ],
     "rules": {
         "indent": [
-            "error",
+            "warn",
             2
         ],
         "linebreak-style": [
-            "error",
+            "warn",
             "unix"
         ],
         "quotes": [
-            "error",
+            "warn",
             "single"
         ],
         "semi": [
-            "error",
+            "warn",
             "always"
         ],
         "mocha/no-exclusive-tests": "error",
