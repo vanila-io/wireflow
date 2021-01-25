6c1fe3a07833bb3508b931c8da79694d257b70e9
Teeranai.P <minibugdev@gmail.com>
2021-01-25 20:22:29 +0700

    Allow to search components (#1)

--- src/components/FlowItemPanel/index.js
+++ src/components/FlowItemPanel/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ItemPanel } from 'gg-editor';
-import Card from 'antd/es/card';
+import { Card, Input } from 'antd/es';
 import 'antd/es/card/style/css';
 
 import NodeItem from './NodeItem';
@@ -8,10 +8,19 @@ import nodes from './nodesData';
 import './style.css';
 
 const FlowItemPanel = () => {
+  const [items, setItems] = useState(nodes);
+
+  function onChange(e) {
+    const keyword = e.target.value.toLowerCase();
+    const searchResults = nodes.filter(n => n.label.toLowerCase().includes(keyword));
+    setItems(searchResults);
+  }
+
   return (
-    <ItemPanel className='sidebar-wrapper'>
+    <ItemPanel className='sidebar-wrapper'> 
       <Card className='sidebar' bodyStyle={{ padding: 0 }}>
-        {nodes && nodes.map((item, i) => <NodeItem key={i} {...item} />)}
+        <Input.Search className='sidebar-search' placeholder='Search' allowClear size='small' onChange={onChange} />
+        {items && items.map((item, i) => <NodeItem key={i} {...item} />)}
       </Card>
     </ItemPanel>
   );
