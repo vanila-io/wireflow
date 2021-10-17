bf4b7a11b3f03a3ccd1b108b233286bc4e78b59f
nwebster1 <Nick_Webster@intuit.com>
2021-10-17 20:46:21 +0200

    add import and export feature

--- src/components/ExportCanvas/index.js
+++ src/components/ExportCanvas/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Button from 'antd/es/button';
+import Dropdown from 'antd/es/dropdown';
+import Menu from 'antd/es/menu';
+import { MoreOutlined, ExportOutlined, ImportOutlined } from '@ant-design/icons';
 import 'antd/es/button/style/css';
+import 'antd/es/dropdown/style/css';
+import 'antd/es/menu/style/css';
 import htmlToImage from 'html-to-image';
 import { ContextMenu, Command, CanvasMenu } from 'gg-editor';
 
 import IconFont from '../IconFont';
 import './style.css';
 
-const ExportCanvas = () => {
+const ExportCanvas = () => {  
   function saveCanvas() {
     htmlToImage
       .toJpeg(document.getElementById('canvas_1'), { quality: 1 })
@@ -19,18 +24,68 @@ const ExportCanvas = () => {
       });
   }
 
+  function exportCanvas() {
+    const data = localStorage.getItem('data');    
+    const blob = new Blob([data], {type: 'application/json'})    
+    const link = document.createElement('a');
+    link.download = 'wireflow.json';
+    link.href = URL.createObjectURL(blob);
+    link.click();
+  }  
+
+  function importCanvas(event, b) {    
+    const r = new FileReader();
+    const file = event.target.files[0];    
+    r.onloadend = function(){      
+      const data = atob(r.result.replace('data:application/json;base64,', ''));
+      localStorage.setItem('data', data);      
+      window.location.reload();
+    }
+    r.readAsDataURL(file);
+  }
+
+  const menu = (
+    <Menu
+      className='export-menu'
+    >
+      <Menu.Item
+        onClick={saveCanvas}
+      >
+        Export Image
+        <IconFont type='icon-upload-demo' />       
+      </Menu.Item>
+      <Menu.Item>
+        <span className='import-input-container'>
+          <input type='file' onChange={(event) => importCanvas(event)} />
+          Import Configuration
+        </span>
+        <ImportOutlined />
+      </Menu.Item>
+      <Menu.Item
+        onClick={exportCanvas}
+      >
+        Export Configuration
+        <ExportOutlined />
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
     <ContextMenu>
       <CanvasMenu>
         <Command name='autoZoom'>
           <div className='export'>
-            <Button
-              onClick={saveCanvas}
-              type='dashed'
-              size='large'
-              shape='circle'
-              icon={<IconFont type='icon-upload-demo' />}
-            />
+            <Dropdown
+              overlay={menu}
+            >
+              <Button                
+                type='dashed'
+                size='large'
+                shape='circle'                
+              >
+                <MoreOutlined />
+              </Button>  
+            </Dropdown>            
           </div>
         </Command>
       </CanvasMenu>
